9f477b83967d7b7c5152d478623009cee14fe3ac
AKSHAY TANPURE <akshaytanpure2210@gmail.com>
2022-10-20 16:05:32 +0530

    updated by Akshay

--- CRM/Customermanager.cs
+++ CRM/Customermanager.cs
@@ -1,24 +1,25 @@
-/*using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Collections.Generic;
 using System.IO;
+using Common;
 namespace CRM;
 
 public class CustomerManager
 {
 
     public static List<Customer> Initialize(){
-     Customer c1=new Customer{ CusromerId="m101", FirstName="Akshay", LastName="Tanpure",   ContactNumber="658952166",Email="tanpure@gmail.com", Id="101"};
-     Customer c2=new Customer{  CusromerId="m102" , FirstName="Rajesh", LastName="Bhosale",  ContactNumber="5564198",Email="bhosale@gmail.com",   Id="102"};
-     Customer c3=new Customer{   CusromerId="m103" ,  FirstName="Rohit", LastName="Patil",    ContactNumber="42867276",Email="patil@gmail.com",     Id="103"};
-     Customer c4=new Customer{    CusromerId="m104" ,  FirstName="Yash",   LastName="Maske",   ContactNumber="77686424",Email="yash.maske@gmail.com", Id="104"};
+     Customer c1=new Customer{ CustomerId="m101",   FirstName="Akshay", LastName="Tanpure",   ContactNumber="658952166",Email="tanpure@gmail.com" };
+     Customer c2=new Customer{  CustomerId="m102" ,  FirstName="Rajesh", LastName="Bhosale",  ContactNumber="5564198",Email="bhosale@gmail.com"};
+     Customer c3=new Customer{   CustomerId="m103" ,   FirstName="Rohit", LastName="Patil",    ContactNumber="42867276",Email="patil@gmail.com"    };
+     Customer c4=new Customer{    CustomerId="m104" ,   FirstName="Yash",   LastName="Maske",   ContactNumber="77686424",Email="yash.maske@gmail.com"};
 
-     List<Customer>customers=new List<Customer>();
-     customers.Add(c1);
-     customers.Add(c2);
-     customers.Add(c3);
-     customers.Add(c4);
-     return customers;
+     List<Customer>customer=new List<Customer>();
+     customer.Add(c1);
+     customer.Add(c2);
+     customer.Add(c3);
+     customer.Add(c4);
+     return customer;
         
     }
   List<Customer>customers=new List<Customer>();
@@ -44,18 +45,18 @@ public class CustomerManager
 
     }
 
-    public static bool Update(Customer cst){
+    public static bool Update(Customer customer){
      
      bool status=false;
      List<Customer> allCustomers=ReStore("customers.json");
      foreach(Customer theCustomer in allCustomers)
      {
-          if(theCustomer.Id== cst.Id )
+          if(theCustomer.CustomerId == customer.CustomerId )
           {
-             theCustomer.FirstName=cst.FirstName;
-             theCustomer.LastName=cst.LastName;
-             theCustomer.ContactNumber=cst.ContactNumber;
-             theCustomer.Email=cst.Email;
+             theCustomer.FirstName=customer.FirstName;
+             theCustomer.LastName=customer.LastName;
+             theCustomer.ContactNumber=customer.ContactNumber;
+             theCustomer.Email=customer.Email;
              Store("customers.json",allCustomers);
              status=true;
           }
@@ -77,7 +78,7 @@ public class CustomerManager
         List<Customer> allCustomers=ReStore("customers.json");
         Customer foundCustomer=null;
         foreach(Customer theCustomer in allCustomers){
-         if (theCustomer.Id==id)
+         if (theCustomer.CustomerId==id)
          {
             foundCustomer=theCustomer;
          }
@@ -92,12 +93,12 @@ public class CustomerManager
     }
 
     
-    public static bool Delete(string cstId)
+    public static bool Delete(string customerId)
     {
         bool status=false;
         List<Customer> allCustomers=ReStore("customers.json");
         foreach(Customer theCustomer in allCustomers){
-         if (theCustomer.Id==cstId)
+         if (theCustomer.CustomerId==customerId)
             {
               allCustomers.Remove(theCustomer);
               Store("customers.json",allCustomers);
@@ -107,6 +108,4 @@ public class CustomerManager
         }
         return status;
     }
-
-
-}   */
+}
