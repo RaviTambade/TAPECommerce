dfae004cc341042f871fd6279488bb9764970538
Ravi Tambade <ravi.tambade@transflower.in>
2022-10-22 04:05:50 +0530

    Using LINQ in .net

---
+++ Resources/SimpleLINQDemoApp/Student.cs
@@ -0,0 +1,14 @@
+﻿ 
+
+namespace LINQDemoApp
+{
+
+    //POCO: Plain Old CLR Object
+    public  class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int CourseID { get; set; }
+    }
+}
