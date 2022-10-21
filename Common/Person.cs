05aefa242039be595357b9447c026cf1ca29a1d9
nutannavale63 <navalenutan63@gmail.com>
2022-10-21 15:26:50 +0530

    abc

--- Common/Person.cs
+++ Common/Person.cs
@@ -1,8 +1,14 @@
-﻿namespace Common;
+﻿namespace Common
+{
 public class Person
 {
         public  string FirstName{get;set;}
         public  string LastName{get;set;}
         public  string Email{get;set;}
         public  string ContactNumber{get;set;}
+
+        public string Id{get;set;}
+
+        
+}
 }
