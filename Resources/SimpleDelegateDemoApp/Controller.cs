6c41a6672afb7aebaa2d9d2f78cd199eaa6cdecf
Ravi Tambade <ravi.tambade@transflower.in>
2022-10-22 02:28:47 +0530

    C# Delegat t    public delegate void TaxHandler(double amount); concept

---
+++ Resources/SimpleDelegateDemoApp/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace SimpleDelegateDemoApp
+{
+    //Hollywood Design Principle
+    //Don't call me , I will call you.
+
+
+    public delegate void TaxHandler(double amount);
+    public delegate void dispatcher(string name, string body);
+
+    internal class Controller
+    {
+        //set of helper functions
+        //set of Callback functions
+        //set of Action Handlers
+        //set of event handlers
+        //set of Action listeners
+        //set of Hook function
+
+        public static void PayIncomeTax(double amount)
+        {
+            Console.WriteLine("Income Tax has been automatically deducted from your Account : {0} Rs.", amount);
+        }
+        public static void PayTDS(double money)
+        {
+            Console.WriteLine("TDS has been automatically deducted from your Account : {0} Rs.", money);
+
+        }
+        public static void SendEmail(string email, string message)
+        {
+            Console.WriteLine("Email has been sent to respective person");
+        }
+        public static void SendSMS(string contactNumber, string message)
+        {
+            Console.WriteLine("Email has been sent to respective person");
+        }
+    }
+}
---
+++ Resources/SimpleDelegateDemoApp/Program.cs
@@ -0,0 +1,52 @@
+﻿//Function Pointer
+    //ISR : Interrupt Service Routine
+    //IVT : Interrupt Vector Table
+
+    //What do you mean by Delegate:
+    // It a object oriented  managed typsafe function Pointer
+    // it a .net object wrapper for function Pointer
+
+
+
+  //Rules for defininf delegate:
+    // 1.return type is common
+    // 2.input type is also common
+    // 3.signature input and output variable types are common
+
+    using SimpleDelegateDemoApp;
+
+ 
+
+    double amount = 5000;
+
+
+    //address of the function will be resolved at compile time
+    //Direct Call
+        //payIncomeTax(amount);  //Static linking, early binding, compile binding
+    
+    //late binding, Dynamic linking, runtime binding
+
+    //Address of function will be resolved at runtime
+
+    //Register address of methods with Delegate instances
+    //Callback function Mapping
+    //Dispatcher registration with callback function
+
+    //Boilar plate Code
+    TaxHandler handler = new TaxHandler(Controller.PayIncomeTax);
+    dispatcher emailDispatcher = new dispatcher(Controller.SendEmail);
+    dispatcher smsDispatcher = new dispatcher(Controller.SendSMS);
+
+    //handler object acting like Proxy
+    //Substitute, agent, broker,
+    //Indirect Call
+    
+    //Invoke Delegate instances
+
+    //Flow of your application
+    //process of your application
+
+    handler.Invoke(amount);
+    
+    emailDispatcher.Invoke("ravi.tambade@transflower.in", "Hi Ravi");
+    smsDispatcher.Invoke("9881735801", "Hi Ravi this imp. message");
