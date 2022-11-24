6240e3d0e384595b1a0c9edaf93100bd1dde4ffd
unknown <ravi.tambade@transflower.in>
2022-11-24 17:33:38 +0530

    Comparable implementation

---
+++ Resources/Dotnet/ComparisionApp/Player.cs
@@ -0,0 +1,61 @@
+namespace IPL;
+using System.Collections;
+
+public  class Player:IComparable {
+    public string name;
+    public int runs;
+    public int matches;
+    public int age;
+    public Player(string name, int age, int matches, int runs){
+        this.name=name;
+        this.age=age;
+        this.matches=matches;
+        this.runs=runs;
+    }
+
+    public int CompareTo(object? obj)
+    {
+        //Typing
+        Player anotherPlayer= (Player) obj;
+
+        //comparision is based on runs scored
+        /* if(this.runs > anotherPlayer.runs){
+            return 1;
+        }
+        else if (this.runs < anotherPlayer.runs){
+            return -1;
+        }
+        else
+        return 0;
+         
+         */
+
+         //comparision is based on age 
+        /*if(this.age > anotherPlayer.age){
+            return 1;
+        }
+        else if (this.age < anotherPlayer.age){
+            return -1;
+        }
+        else
+        return 0;*/
+
+         //comparision is based on matches played
+         if(this.matches > anotherPlayer.matches){
+            return 1;
+        }
+        else if (this.matches < anotherPlayer.matches){
+            return -1;
+        }
+        else
+        return 0;
+         
+    }
+
+
+
+    public override string ToString()
+    {
+        return    this.name + " "+ this.age + " "+ this.runs + "  "+ this.matches;
+    }
+}
---
+++ Resources/Dotnet/ComparisionApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using IPL;
+using System.Collections.Generic;
+
+
+Player p1= new Player("Sourav Ganguly",50, 300, 50000);
+Player p2= new Player("Sachin Tendulkar",49, 350, 100000);
+Player p3= new Player("Rahul Dravid",48, 250, 70000);
+
+List<Player> indianPlayers=new List<Player>();
+indianPlayers.Add(p1);
+indianPlayers.Add(p2);
+indianPlayers.Add(p3);
+
+/**
+    //This does not work for user defined types
+    if( p1 < p2){
+
+    }
+*/
+
+int result= p1.CompareTo(p2);
+
+if(result ==1){
+    Console.WriteLine( " Sourav is greater than Sachin");
+}
+else if (result ==-1){
+    Console.WriteLine( " Sachin is greater than Sourav");
+}
+else{
+    Console.WriteLine( " Sourav and  Sachin are having same runs");
+}
+
+
+int num1=34;
+int num2=56;
+//double, int, float, long, short  will work
+
+    if(num1 < num2){
+        Console.WriteLine( "First number is smaller");
+    }
+    else{
+    Console.WriteLine( "First number is  not smaller");
+    }
+
+
+Console.WriteLine( "Before sorting players");
+foreach( Player currentPlayer in indianPlayers){
+    /*Console.WriteLine( " " + currentPlayer.name + " "+ currentPlayer.age + " "+
+                             currentPlayer.runs + "  "+ currentPlayer.matches);
+                             */
+    Console.WriteLine(currentPlayer);
+}
+
+//it (sort) is inbuilt function of List class
+
+indianPlayers.Sort();
+Console.WriteLine( " \nAfter sorting players");
+
+foreach( Player currentPlayer in indianPlayers){
+   /* Console.WriteLine( " " + currentPlayer.name + " "+ currentPlayer.age + " "+
+                             currentPlayer.runs + "  "+ currentPlayer.matches); */
+    Console.WriteLine(currentPlayer);
+}
