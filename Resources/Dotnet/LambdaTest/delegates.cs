a1cae370628707128f3ccbd5ccda7520f88b98c9
unknown <ravi.tambade@transflower.in>
2022-11-25 06:05:34 +0530

    Lambda Expression

---
+++ Resources/Dotnet/LambdaTest/delegates.cs
@@ -0,0 +1,6 @@
+namespace TFLTypes;
+delegate void doSomething(); 
+delegate int anonymousOperation1(int i); 
+delegate int anonymousOperation2(int a, int b);  // act like a function pointer
+// Object Oriented typesafe managed function Pointer
+
