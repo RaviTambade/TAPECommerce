f939aa47bfab741ff48f266d7c522a6b4c6fcba6
unknown <ravi.tambade@transflower.in>
2022-11-26 19:52:48 +0530

    LINQ SetOperators

---
+++ Resources/Dotnet/LinqSetOperatorsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using SetOperators;
+
+//Invoke the linq set operator examples within the project
+SetDistinct1.MethodSyntaxExample();
+
+SetDistinct2.QuerySyntaxExample();
+SetDistinct2.MethodSyntaxExample();
+
+SetUnion1.MethodSyntaxExample();
+
+SetUnion2.MethodSyntaxExample();
+
+SetIntersect1.MethodSyntaxExample();
+
+SetIntersect2.MethodSyntaxExample();
+
+SetExcept1.MethodSyntaxExample();
+
+SetExcept2.MethodSyntaxExample();
