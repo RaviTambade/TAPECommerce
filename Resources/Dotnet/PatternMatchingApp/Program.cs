f2b58498f4d3bcc3818fdbc0e03897837a5e5bed
unknown <ravi.tambade@transflower.in>
2022-11-26 18:46:04 +0530

    Pattern Matching

---
+++ Resources/Dotnet/PatternMatchingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using PatternMatching;
+
+var s = Tryout.CreateShape("square");
+var c = Tryout.CreateShape("circle");
+
+Console.WriteLine(GeometricUtilities.ComputeArea(s));
+Console.WriteLine(GeometricUtilities.ComputeArea(c));
+
+Console.WriteLine(GeometricUtilities.ComputeAreaModernIs(s));
+Console.WriteLine(GeometricUtilities.ComputeAreaModernIs(c));
+
+Console.WriteLine(GeometricUtilities.ComputeArea_Version3(s));
+Console.WriteLine(GeometricUtilities.ComputeArea_Version3(c));
+
+var what = Tryout.CreateShape("       ");
+Console.WriteLine(what);
+
+var wrong = Tryout.CreateShape("trapezoid");
+Console.WriteLine(wrong);
+
+       
---
+++ Resources/Dotnet/PatternMatchingApp/Shapes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatching
+{
+    #region 01_ShapeDefinitions
+    public class Square
+    {
+        public double Side { get; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+    }
+    public class Circle
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+    }
+    public struct Rectangle
+    {
+        public double Length { get; }
+        public double Height { get; }
+
+        public Rectangle(double length, double height)
+        {
+            Length = length;
+            Height = height;
+        }
+    }
+    public class Triangle
+    {
+        public double Base { get; }
+        public double Height { get; }
+
+        public Triangle(double @base, double height)
+        {
+            Base = @base;
+            Height = height;
+        }
+    }
+    #endregion
+
+    public class Tryout{
+
+        public  static object CreateShape(string shapeDescription)
+        {
+            switch (shapeDescription)
+            {
+                case "circle":
+                    return new Circle(2);
+
+                case "square":
+                    return new Square(4);
+
+                case "large-circle":
+                    return new Circle(12);
+
+                case var o when (o?.Trim().Length ?? 0) == 0:
+                    // white space
+                    return null;
+                default:
+                    return "invalid shape description";
+            }
+        }
+      
+    }
+}
