f2f4caed7880854d60917b92f14a1c31d621782d
unknown <ravi.tambade@transflower.in>
2022-11-26 19:25:30 +0530

    Linq Grouping Ordering Partitionning

---
+++ Resources/Dotnet/LinqGroupingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grouping;
+//Invoke the linq grouping examples within the project
+GroupBy1.QuerySyntaxExample();
+GroupBy1.MethodSyntaxExample();
+
+GroupBy2.QuerySyntaxExample();
+GroupBy2.MethodSyntaxExample();
+
+GroupBy3.QuerySyntaxExample();
+GroupBy3.MethodSyntaxExample();
+
+GroupByNested1.QuerySyntaxExample();
+
+GroupByComparer1.MethodSyntaxExample();
+
+GroupByComparer2.MethodSyntaxExample();
---
+++ Resources/Dotnet/LinqRestricationApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Restriction;
+
+//Invoke the linq restriction examples within the project
+WhereClause1.QuerySyntaxExample();
+WhereClause1.MethodSyntaxExample();
+
+WhereClause2.QuerySyntaxExample();
+WhereClause2.MethodSyntaxExample();
+
+WhereClause3.QuerySyntaxExample();
+WhereClause3.MethodSyntaxExample();
+
+WhereClause4.Example();
