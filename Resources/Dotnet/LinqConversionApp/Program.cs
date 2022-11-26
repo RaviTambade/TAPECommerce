15136b7d0207edb8f6a692385e926ff6a4850c20
unknown <ravi.tambade@transflower.in>
2022-11-26 19:57:08 +0530

    LINQ Conversion

---
+++ Resources/Dotnet/LinqConversionApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Conversion;
+
+ToArraySample1.MethodSyntaxExample();
+ToArraySample1.QuerySyntaxExample();
+
+ToListSample1.MethodSyntaxExample();
+ToListSample1.QuerySyntaxExample();
+
+ToDictionarySample1.Example();
+
+OfTypeSample1.Example();
