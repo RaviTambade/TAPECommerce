b35a9dbcad8e5563ffe8cd2d85635f24ee4967e0
unknown <ravi.tambade@transflower.in>
2022-12-07 11:42:11 +0530

    CRUD Operation for mysql table

--- Resources/Dotnet/DDLMySQLApp/Program.cs
+++ Resources/Dotnet/DDLMySQLApp/Program.cs
@@ -1,31 +1,11 @@
-﻿using MySql.Data.MySqlClient ;
-string conString=@"server=localhost;user=root;database=ecommerce;port=3306;password=password";
+﻿
+using DAL;
 
-MySqlConnection con=new MySqlConnection();
-con.ConnectionString=conString;
-try{
-    /* DDL Command */
-    //string query="CREATE TABLE tflroles (roleid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  title VARCHAR(25))";
-    //string query="DROP TABLE tflroles";
-    //string query="CREATE TABLE tflroles (roleid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  title VARCHAR(25))";
-    /* DML Command */
+//Student table CRUD Operations
 
-    //string query="INSERT INTO tflroles (title) values('Trainer')";
-    //string query="DELETE from tflroles  WHERE title='Trainer'";
-    //string query="UPDATE tflroles SET title='CEO' WHERE roleid=1";
-    Console.WriteLine("Please enter  new Role:");
-    string role=Console.ReadLine();
-    string query="INSERT INTO tflroles (title) values('"+ role+ "')";
-
-    MySqlCommand cmd=new MySqlCommand(query,con);
-    con.Open();
-    cmd.ExecuteNonQuery();  // 
-}
-catch(Exception ee){
-    Console.WriteLine(ee.Message);
-
-}
-finally{
-    con.Close();
-}
+//MySQLDBManager.CreateTable();
+MySQLDBManager.Insert("Rohit", "Gore","BE Mech");
+MySQLDBManager.Insert("Pargati", "Bangar","BE Elect");
+MySQLDBManager.Insert("Abhay", "Navale","BCS");
+MySQLDBManager.ShowAllStudents();
 
